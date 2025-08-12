e6a91521fb2c41ac092b7d04293755f9bc57fad0
Nitesh Yadav <niteshsultaniya63@gmail.com>
2025-08-12 13:42:34 +0530

    change

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const data =[
     {name:'Prateek Balothia',Email:'Prateek@test.com'},
     {name:'Pankaj Jangid',Email:'Pankaj@test.com'},
-    {name:'Nitesh Yadav',Email:'Nitesh@test.com'},
+    {name:'Nitesh Yadav',Email:'Nitesh@test.comm'},
 
 ] 
 module.exports=data;
